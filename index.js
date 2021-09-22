285771f6de44ec9025cc607d370ff7ce2dc051d5
Maarten Mollee <72136578+Maarten-Mollee@users.noreply.github.com>
2021-09-22 09:18:36 +0200

    finished lesson 2/3: blackjack (javascript file)

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let player = {
+    name: "Riznir",
+    chips: 145
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+function getRamdonCard () {
+    let number = Math.ceil(Math.random() * 11)
+    if (number === 1) {
+        return 11
+    } else if (number > 10) {
+        return 10
+    }
+    return number
+}
+
 function startGame () {
-    renderGame()
+    let firstCard = getRamdonCard()
+    let secondCard = getRamdonCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    isAlive = true
+    hasBlackJack = false
+    renderGame ()
 }
 
 function renderGame () {
+    cardsEl.textContent = "cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     if (sum <= 20) {
         message = "do you want to draw a new card?"
     } else if (sum === 21) {
@@ -26,20 +52,13 @@ function renderGame () {
     }
     messageEl.textContent = message
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
 }
 
 function newCard () {
-    let newCard = 8
-    cards.push(newCard)
-    sum += newCard
-    
-    renderGame ()
+    if (isAlive === true && hasBlackJack === false) {
+        let newCard = getRamdonCard()
+        cards.push(newCard)
+        sum += newCard
+        renderGame ()
+    }
 }
-
-let cardz = [7, 3, 9]
-
-for (let i = 0; i < cardz.length; i++) {
-    console.log(cardz[i])
-}
-
